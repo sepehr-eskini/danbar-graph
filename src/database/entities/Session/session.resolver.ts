e05acf1f25c6b930766486928a1ae55004e651a1
sepehr-eskini <sepehr.eskini@gmail.com>
2026-06-21 18:58:15 +0330

    Feat: Refactor session fetching logic to use query builder for improved flexibility and performance

--- src/database/entities/Session/session.resolver.ts
+++ src/database/entities/Session/session.resolver.ts
@@ -4,38 +4,59 @@ import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware } from "type-graphql
 
 import { Admin } from "../Admin"
 import { Session } from "./session.entity"
-import { CreateSessionRq, FetchSessionListRq, ToggleSessionStatus } from "./session.rq"
+import { CreateSessionRq, FetchActiveSessionListRq, FetchSessionListRq, ToggleSessionStatus } from "./session.rq"
 
 @Resolver()
 export class SessionResolver {
     @Query(() => [Session])
     @UseMiddleware([AuthMiddleware])
     async fetchSessionList(@Arg("body") { day, time_period_token }: FetchSessionListRq): Promise<Session[]> {
-        const sessions = await Session.find({
-            where: {
-                ...(day && { day }),
-                ...(time_period_token && { time_period_token }),
-            },
-            order: {
-                is_active: "DESC",
-                created_at: "DESC",
-            },
-        })
+        let query = Session.createQueryBuilder("session").leftJoinAndSelect("session.time_period", "time_period")
+
+        if (day) query = query.andWhere("session.day = :day", { day })
+        if (time_period_token)
+            query = query.andWhere("session.time_period_token = :time_period_token", { time_period_token })
+
+        const sessions = await query
+            .orderBy("session.is_active", "DESC")
+            .addOrderBy(
+                `CASE
+            WHEN session.day = 'SAT' THEN 0
+            WHEN session.day = 'SUN' THEN 1
+            WHEN session.day = 'MON' THEN 2
+            WHEN session.day = 'TUE' THEN 3
+            WHEN session.day = 'WED' THEN 4
+            WHEN session.day = 'THU' THEN 5
+            WHEN session.day = 'FRI' THEN 6
+        END`,
+                "ASC",
+            )
+            .addOrderBy("time_period.from_time", "ASC")
+            .getMany()
 
         return sessions
     }
 
     @Query(() => [Session])
     @UseMiddleware([AuthMiddleware])
-    async fetchActiveSessionList(@Arg("body") { day, time_period_token }: FetchSessionListRq): Promise<Session[]> {
-        const sessions = await Session.find({
-            where: {
-                ...(day && { day }),
-                ...(time_period_token && { time_period_token }),
-                is_active: true,
-            },
-            order: { created_at: "DESC" },
-        })
+    async fetchActiveSessionList(
+        @Arg("body") { day, time_period_token }: FetchActiveSessionListRq,
+    ): Promise<Session[]> {
+        let query = Session.createQueryBuilder("session")
+            .leftJoinAndSelect("session.time_period", "time_period")
+            .andWhere("session.is_active = :is_active", { is_active: true })
+
+        if (day) query = query.andWhere("session.day = :day", { day })
+        if (time_period_token)
+            query = query.andWhere("session.time_period_token = :time_period_token", { time_period_token })
+
+        const sessions = await query
+            .orderBy(
+                "CASE WHEN session.day = 'SAT' THEN 0 WHEN session.day = 'SUN' THEN 1 WHEN session.day = 'MON' THEN 2 WHEN session.day = 'TUE' THEN 3 WHEN session.day = 'WED' THEN 4 WHEN session.day = 'THU' THEN 5 WHEN session.day = 'FRI' THEN 6 END",
+                "ASC",
+            )
+            .addOrderBy("time_period.from_time", "ASC")
+            .getMany()
 
         return sessions
     }
