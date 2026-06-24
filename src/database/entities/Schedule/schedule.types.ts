8887644789c3c77a1f2a1b6bc6953c838ea037cc
sepehr-eskini <sepehr.eskini@gmail.com>
2026-06-24 13:22:34 +0330

    Add OFFSET status to E_ScheduleStatus enum for improved schedule management

--- src/database/entities/Schedule/schedule.types.ts
+++ src/database/entities/Schedule/schedule.types.ts
@@ -3,6 +3,7 @@ import { registerEnumType } from "type-graphql"
 export enum E_ScheduleStatus {
     UNSET = "UNSET",
     PRESENT = "PRESENT",
+    OFFSET = "OFFSET",
     ABSENT = "ABSENT",
     CANCEL = "CANCEL",
 }
